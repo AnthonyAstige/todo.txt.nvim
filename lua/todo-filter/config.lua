83387012ec42d990a62067ba8f5d486127cc2ea1
Anthony Astige <Anthony@Astige.com>
2025-04-19 19:21:53 -0500

    Lua format and my default location in setup just for POC

--- lua/todo-filter/config.lua
+++ lua/todo-filter/config.lua
@@ -2,20 +2,20 @@
 local M = {}
 
 M.defaults = {
-  -- Path to your todo.txt file
-  todo_file = vim.fn.expand('~/todo.txt'),
+	-- Path to your todo.txt file
+	todo_file = vim.fn.expand("~/todo.txt/todo.txt"),
 
-  -- Keymaps for filtering actions
-  keymaps = {
-    project = '<leader>tfp', -- Filter by Project
-    context = '<leader>tfc', -- Filter by Context
-    clear   = '<leader>tfx', -- Clear filter (eXpand folds)
-  },
+	-- Keymaps for filtering actions
+	keymaps = {
+		project = "<leader>tfp", -- Filter by Project
+		context = "<leader>tfc", -- Filter by Context
+		clear = "<leader>tfx", -- Clear filter (eXpand folds)
+	},
 
-  -- Filetypes to activate folding and commands for
-  -- Ensure 'todo' or similar is set for your todo.txt files
-  -- e.g., via vim.filetype.add() or an ftplugin
-  filetypes = { 'todo', 'todos', 'todo.txt' },
+	-- Filetypes to activate folding and commands for
+	-- Ensure 'todo' or similar is set for your todo.txt files
+	-- e.g., via vim.filetype.add() or an ftplugin
+	filetypes = { "todo", "todos", "todo.txt" },
 }
 
 return M
