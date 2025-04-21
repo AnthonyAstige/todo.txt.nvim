6374068aec64bb823b9dacc515486aaf2bafc704
Anthony Astige <Anthony@Astige.com>
2025-04-20 22:13:02 -0500

    Useless comments removed

--- lua/todo_txt/commands.lua
+++ lua/todo_txt/commands.lua
@@ -5,9 +5,7 @@ local fn = vim.fn
 local tags = require("todo_txt.tags")
 local folding = require("todo_txt.folding")
 
--- Creates the user commands and autocommands.
 function M.create_commands(cfg)
-	-- Command to filter by project
 	api.nvim_create_user_command("TodoTxtFilterProject", function()
 		local items = tags.scan_tags("%+", cfg.todo_file)
 		if #items == 0 then
@@ -16,14 +14,12 @@ function M.create_commands(cfg)
 		end
 		vim.ui.select(items, { prompt = "Project> ", kind = "todo_project" }, function(selected)
 			if selected then
-				-- Escape '+' for Lua pattern matching and create the search pattern
 				vim.g.todo_txt_project_pattern = "+" .. fn.escape(selected, "+")
 				folding.refresh_folding()
 			end
 		end)
 	end, { desc = "Filter todo list by project (+Tag) using vim.ui.select" })
 
-	-- Command to filter by context
 	api.nvim_create_user_command("TodoTxtFilterContext", function()
 		local items = tags.scan_tags("@", cfg.todo_file)
 		if #items == 0 then
@@ -32,14 +28,12 @@ function M.create_commands(cfg)
 		end
 		vim.ui.select(items, { prompt = "Context> ", kind = "todo_context" }, function(selected)
 			if selected then
-				-- Escape '@' for Lua pattern matching and create the search pattern
 				vim.g.todo_txt_context_pattern = "@" .. fn.escape(selected, "@")
 				folding.refresh_folding()
 			end
 		end)
 	end, { desc = "Filter todo list by context (@Tag) using vim.ui.select" })
 
-	-- Command to clear the filter
 	api.nvim_create_user_command("TodoTxtFilterClear", function()
 		vim.g.todo_txt_context_pattern = ""
 		vim.g.todo_txt_project_pattern = ""
