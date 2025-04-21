66d51c22d7997429ff63715592df68243fd960ed
Anthony Astige <Anthony@Astige.com>
2025-04-21 12:53:50 -0500

    Fix hyperfocus thank you clause 3.7

--- lua/todo_txt/hyperfocus.lua
+++ lua/todo_txt/hyperfocus.lua
@@ -4,15 +4,13 @@ local enabled = false
 
 local function enable_myopic()
 	vim.opt_local.conceallevel = 2
-	-- TODO: Make this actually work ... concealcursor doesn't seem to be showing what's under it .. though everythign does hide
-	vim.opt_local.concealcursor = "nvic"
+	vim.opt_local.concealcursor = "" -- Set concealcursor to empty to show text under cursor
 	vim.cmd([[syntax match TodoTxtConceal /./ conceal]])
 	enabled = true
 end
 
 local function disable_myopic()
 	vim.opt_local.conceallevel = 0
-	vim.opt_local.concealcursor = ""
 	vim.cmd([[syntax clear TodoTxtConceal]])
 	enabled = false
 end
