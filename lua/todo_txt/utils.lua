15ee4e02f40c4a47f10751c321a06ca0baad83bd
Anthony Astige <Anthony@Astige.com>
2025-04-23 18:21:29 -0500

    .nvim

--- lua/todo_txt/utils.lua
+++ lua/todo_txt/utils.lua
@@ -5,7 +5,7 @@ local M = {}
 -- @param level vim.log.levels The notification level (e.g., vim.log.levels.INFO).
 -- @param opts table Optional table of extra options for vim.notify.
 function M.notify(msg, level, opts)
-	local default_opts = { title = "todo.txt" }
+	local default_opts = { title = "todo.txt.nvim" }
 	local merged_opts = vim.tbl_deep_extend("force", {}, default_opts, opts or {})
 	vim.notify(msg, level, merged_opts)
 end
